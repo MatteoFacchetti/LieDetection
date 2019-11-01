69b419a8cc55addf43b97c50bcc5a378f98a7c28
matteo <m.facchetti.96@gmail.com>
2019-11-01 20:50:44 +0100

    Updated train-test split

--- src/preprocessing/train_test_split.py
+++ src/preprocessing/train_test_split.py
@@ -3,3 +3,16 @@ from utils import file_utils
 
 run_cfg = file_utils.read_yaml("../../config/run.yaml")
 test_videos = run_cfg["test_videos"]
+
+# Get name of the frames to move to the test folder
+test_frames = []
+for video in test_videos:
+    test_frames.append(list(filter(lambda x: f"_{video}_" in x, os.listdir(f"../../data/0_crop"))))
+print(test_frames)
+
+# Move test frames to the test folder
+for frame in test_frames:
+    try:
+        os.rename(f"../../data/0_crop/{frame}", f"../../data/test/0_crop/{frame}")
+    except FileNotFoundError:
+        os.rename(f"../../data/1_crop/{frame}", f"../../data/test/1_crop/{frame}")
