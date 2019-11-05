be1558a1c9fbb5860efacab88d6bdab12147625b
matteo <m.facchetti.96@gmail.com>
2019-11-05 20:00:54 +0100

    Training the model.

--- src/modelling/training.py
+++ src/modelling/training.py
@@ -1,3 +1,4 @@
+import click
 import logging
 import os
 import numpy as np
@@ -6,47 +7,63 @@ import cv2
 from imutils import paths
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
+from keras.preprocessing.image import ImageDataGenerator
 
 from utils import file_utils
 
-# Logging configuration
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Read configuration files
-run_cfg = file_utils.read_yaml("../../config/run.yaml")
-train = run_cfg["train"]
+@click.command()
+@click.option("--run_config")
+def main(run_config):
+    # Initialize
+    logger = logging.getLogger(__name__)
+    logger.info(f"Run configuration file: {run_config}")
 
-# Grab the list of images and initialize the lists data and images
-logging.info("Loading images...")
-image_paths = list(paths.list_images(train))
-data = []
-labels = []
+    # Read configuration file
+    run_cfg = file_utils.read_yaml(run_config)
+    train = run_cfg["train"]
 
-# Loop over the image paths
-for image_path in image_paths:
-    # Extract the class label from the filename
-    label = image_path.split(os.path.sep)[-2]
+    # Grab the list of images and initialize the lists data and images
+    logger.info("Loading images...")
+    image_paths = list(paths.list_images(train))
+    data = []
+    labels = []
 
-    # Load the image
-    # TODO: reshape e rimappare i colori in base al pretrained model che andiamo a usare
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (224, 224))
+    # Loop over the image paths
+    for image_path in image_paths:
+        # Extract the class label from the filename
+        label = image_path.split(os.path.sep)[-2]
 
-    # Update data and label lists
-    data.append(image)
-    labels.append(label)
-logging.info("Images loaded successfully.")
+        # Load the image
+        # TODO: reshape e rimappare i colori in base al pretrained model che andiamo a usare
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, (224, 224))
 
-# Convert data and labels to numpy arrays
-data = np.array(data)
-labels = np.array(labels)
+        # Update data and label lists
+        data.append(image)
+        labels.append(label)
+    logger.info("Images loaded successfully.")
 
-# Perform one-hot encoding on the labels
-lb = LabelBinarizer()
-labels = lb.fit_transform(labels)
+    # Convert data and labels to numpy arrays
+    data = np.array(data)
+    labels = np.array(labels)
 
-# TODO: Train-test split (80%-20%) - Forse non serve, avendo già splittato a monte
-(X_train, X_test, y_train, y_test) = train_test_split(data, labels, test_size=0.2, stratify=labels, random_state=42)
+    # Perform one-hot encoding on the labels
+    lb = LabelBinarizer()
+    labels = lb.fit_transform(labels)
 
-# TODO: Initialize data augmentation object
+    # TODO: Train-test split (80%-20%) - Forse non serve, avendo già splittato a monte
+    (X_train, X_test, y_train, y_test) = train_test_split(data, labels, test_size=0.2, stratify=labels, random_state=42)
+
+    # TODO: Initialize data augmentation object
+    train_aug = ImageDataGenerator(
+
+    )
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    main()
--- src/preprocessing/train_test_split.py
+++ src/preprocessing/train_test_split.py
@@ -1,21 +1,41 @@
+import click
+import logging
 import os
 import itertools
 
 from utils import file_utils
 
-run_cfg = file_utils.read_yaml("../../config/run.yaml")
-test_videos = run_cfg["test_videos"]
 
-# Get name of the frames to move to the test folder
-test_frames = []
-for video in test_videos:
-    test_frames.append(list(filter(lambda x: f"_{video}_" in x, os.listdir(f"../../data/train/1_crop"))))
-    test_frames.append(list(filter(lambda x: f"_{video}_" in x, os.listdir(f"../../data/train/0_crop"))))
-test_frames = list(itertools.chain(*test_frames))
+@click.command()
+@click.option("--run_config")
+def main(run_config):
+    # Initialize
+    logger = logging.getLogger(__name__)
+    logger.info(f"Run configuration file: {run_config}")
 
-# Move test frames to the test folder
-for frame in test_frames:
-    try:
-        os.rename(f"../../data/train/1_crop/{frame}", f"../../data/test/1_crop/{frame}")
-    except FileNotFoundError:
-        os.rename(f"../../data/train/0_crop/{frame}", f"../../data/test/0_crop/{frame}")
+    # Read configuration file
+    run_cfg = file_utils.read_yaml(run_config)
+    train_path = run_cfg["train"]
+    test_path = run_cfg["test"]
+    test_videos = run_cfg["test_videos"]
+
+    # Get name of the frames to move to the test folder
+    test_frames = []
+    for video in test_videos:
+        test_frames.append(list(filter(lambda x: f"_{video}_" in x, os.listdir(f"{train_path}/1_crop"))))
+        test_frames.append(list(filter(lambda x: f"_{video}_" in x, os.listdir(f"{train_path}/0_crop"))))
+    test_frames = list(itertools.chain(*test_frames))
+
+    # Move test frames to the test folder
+    for frame in test_frames:
+        try:
+            os.rename(f"{train_path}/1_crop/{frame}", f"{test_path}/1_crop/{frame}")
+        except FileNotFoundError:
+            os.rename(f"{train_path}/0_crop/{frame}", f"{test_path}/0_crop/{frame}")
+
+
+if __name__ == '__main__':
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    main()
